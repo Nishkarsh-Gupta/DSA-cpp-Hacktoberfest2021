78d8d8c2560a60cc86e6783ab749dc0d71d5df5b
_keen <70911700+Nishkarsh-Gupta@users.noreply.github.com>
2022-07-22 19:39:01 +0530

    Update 2. Lucky Numbers .cpp

--- 02. Mathematics/2. Lucky Numbers .cpp
+++ 02. Mathematics/2. Lucky Numbers .cpp
@@ -1,5 +1,6 @@
 /**
 2. Lucky Numbers 
+lol added
 Lucky numbers are subset of integers. Rather than going into much theory, let us see the process of arriving at lucky numbers,
 Take the set of integers
 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19,……
